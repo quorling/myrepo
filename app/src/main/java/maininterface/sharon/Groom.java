c63b70e159ab9aee09a2b9899e2a59b4dd48ccb5
SharonChanxx <sharon199717@gmail.com>
2021-10-07 15:25:42 +0800

    Peta App

---
+++ app/src/main/java/maininterface/sharon/Groom.java
@@ -0,0 +1,14 @@
+package maininterface.sharon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Groom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_groom);
+    }
+}
