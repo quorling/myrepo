0b16859ad5206d5ca898f6ef5b76ab3f30159ee9
SharonChanxx <sharon199717@gmail.com>
2021-10-09 20:49:29 +0800

    Pictures

---
+++ app/src/main/java/maininterface/sharon/Stuff.java
@@ -0,0 +1,14 @@
+package maininterface.sharon;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Stuff extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stuff);
+    }
+}
